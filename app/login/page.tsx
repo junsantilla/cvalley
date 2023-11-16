5fb4b1b0ebc0737e668ea6eebeac8e1843adad03
junsantilla <junsantilla@live.com>
2023-11-16 18:13:31 +0800

    Remove sign in with email option

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,91 +1,76 @@
-"use client";
-import Link from "next/link";
-import { UserAuthForm } from "@/components/UserAuthFormProps";
-import { UserAuth } from "@/context/AuthContext";
-import { useEffect, useState } from "react";
-import { redirect } from "next/navigation";
+"use client"
+import Link from "next/link"
+import { UserAuthForm } from "@/components/UserAuthFormProps"
+import { UserAuth } from "@/context/AuthContext"
+import { useEffect, useState } from "react"
+import { redirect } from "next/navigation"
 
 export default function Login() {
-	const { user } = UserAuth();
-	const [isLoading, setIsLoading] = useState(true);
+    const { user } = UserAuth()
+    const [isLoading, setIsLoading] = useState(true)
 
-	useEffect(() => {
-		if (user) {
-			setIsLoading(false);
-			const redirectTo = localStorage.getItem("redirectTo");
-			if (redirectTo) {
-				localStorage.removeItem("redirectTo"); // Clear the value
-				redirect(redirectTo);
-			} else {
-				redirect("/dashboard"); // Redirect to the dashboard by default
-			}
-		} else {
-			setIsLoading(false);
-		}
-	}, [user]);
+    useEffect(() => {
+        if (user) {
+            setIsLoading(false)
+            const redirectTo = localStorage.getItem("redirectTo")
+            if (redirectTo) {
+                localStorage.removeItem("redirectTo") // Clear the value
+                redirect(redirectTo)
+            } else {
+                redirect("/cv-builder") // Redirect to the dashboard by default
+            }
+        } else {
+            setIsLoading(false)
+        }
+    }, [user])
 
-	// Render loading state while isLoading is true
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
+    // Render loading state while isLoading is true
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
-	return (
-		<>
-			<div className="md:hidden"></div>
-			<div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-					<div className="absolute inset-0 bg-slate-950 login" />
-					<div className="relative z-20 flex items-center text-lg font-extrabold">
-						<Link href="/" className="cursor">
-							CValley.io
-						</Link>
-					</div>
-					<div className="relative z-20 mt-auto">
-						<blockquote className="space-y-2">
-							<p className="text-lg">
-								Dive into the world of resume perfection with
-								CValley.io. Our platform offers features
-								designed to simplify the resume creation
-								process. Explore a diverse range of easy-to-use
-								templates, and access expert tips and insights
-								that will elevate your resume to a professional
-								masterpiece.
-							</p>
-						</blockquote>
-					</div>
-				</div>
-				<div className="lg:p-8">
-					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-						<div className="flex flex-col space-y-2 text-center">
-							<h1 className="text-2xl font-semibold tracking-tight">
-								Create an account
-							</h1>
-							<p className="text-sm text-muted-foreground">
-								Enter your email below to create your account
-							</p>
-						</div>
-						<UserAuthForm />
-						<p className="px-8 text-center text-sm text-muted-foreground">
-							By signing in, you agree to our
-							<br />
-							<Link
-								href="/terms"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Terms of Service
-							</Link>{" "}
-							and{" "}
-							<Link
-								href="/privacy"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Privacy Policy
-							</Link>
-							.
-						</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="md:hidden"></div>
+            <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+                <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+                    <div className="absolute inset-0 bg-slate-950 login" />
+                    <div className="relative z-20 flex items-center text-lg font-extrabold">
+                        <Link href="/" className="cursor">
+                            CValley.io
+                        </Link>
+                    </div>
+                    <div className="relative z-20 mt-auto">
+                        <blockquote className="space-y-2">
+                            <p className="text-lg">
+                                Dive into the world of resume perfection with CValley.io. Our platform offers features designed to simplify the resume creation process. Explore a diverse range of easy-to-use templates, and access expert tips and insights that will elevate your
+                                resume to a professional masterpiece.
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+                <div className="lg:p-8">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                        <div className="flex flex-col space-y-2 text-center">
+                            <h1 className="text-2xl tracking-tight font-extrabold">Log in to CValley.io</h1>
+                            {/* <p className="text-sm text-muted-foreground">Enter your email below to create your account</p> */}
+                        </div>
+                        <UserAuthForm />
+                        <p className="px-8 text-center text-sm text-muted-foreground">
+                            By signing in, you agree to our
+                            <br />
+                            <Link href="/terms" className="underline underline-offset-4 hover:text-primary">
+                                Terms of Service
+                            </Link>{" "}
+                            and{" "}
+                            <Link href="/privacy" className="underline underline-offset-4 hover:text-primary">
+                                Privacy Policy
+                            </Link>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
--- components/UserAuthFormProps.tsx
+++ components/UserAuthFormProps.tsx
@@ -1,79 +1,48 @@
-"use client";
-import * as React from "react";
-import { UserAuth } from "../context/AuthContext";
-import { cn } from "@/lib/utils";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import { BiLogoGoogle } from "react-icons/bi";
+"use client"
+import * as React from "react"
+import { UserAuth } from "../context/AuthContext"
+import { cn } from "@/lib/utils"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import { BiLogoGoogle } from "react-icons/bi"
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm() {
-	const { user, googleSignIn, logOut } = UserAuth();
-	const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const { user, googleSignIn, logOut } = UserAuth()
+    const [isLoading, setIsLoading] = React.useState<boolean>(false)
 
-	const handleSignIn = async () => {
-		try {
-			await googleSignIn();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    const handleSignIn = async () => {
+        try {
+            await googleSignIn()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-	const handleSignOut = async () => {
-		try {
-			await logOut();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    const handleSignOut = async () => {
+        try {
+            await logOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-	async function onSubmit(event: React.SyntheticEvent) {
-		event.preventDefault();
-		setIsLoading(true);
+    async function onSubmit(event: React.SyntheticEvent) {
+        event.preventDefault()
+        setIsLoading(true)
 
-		setTimeout(() => {
-			setIsLoading(false);
-		}, 3000);
-	}
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 3000)
+    }
 
-	return (
-		<div className="grid gap-6">
-			<form onSubmit={onSubmit}>
-				<div className="grid gap-2">
-					<div className="grid gap-1">
-						<Label className="sr-only" htmlFor="email">
-							Email
-						</Label>
-						<Input
-							id="email"
-							placeholder="name@example.com"
-							type="email"
-							autoCapitalize="none"
-							autoComplete="email"
-							autoCorrect="off"
-							disabled={isLoading}
-						/>
-					</div>
-					<Button disabled={isLoading}>
-						{isLoading ? "Signing In" : "Sign In with Email"}
-					</Button>
-				</div>
-			</form>
-			<div className="relative">
-				<div className="absolute inset-0 flex items-center">
-					<span className="w-full border-t" />
-				</div>
-				<div className="relative flex justify-center text-xs uppercase">
-					<span className="bg-background px-2 text-muted-foreground">
-						Or continue with
-					</span>
-				</div>
-			</div>
-			<Button variant="outline" type="button" onClick={handleSignIn}>
-				<BiLogoGoogle className="mr-1 text-lg" /> Google
-			</Button>
-		</div>
-	);
+    return (
+        <div className="grid gap-2">
+            <Button type="button" onClick={handleSignIn}>
+                <BiLogoGoogle className="mr-1 text-lg" /> Google
+            </Button>
+        </div>
+    )
 }
