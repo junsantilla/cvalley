cd8b4e0f7fbb499c53652d4bf096be67559924c1
junsantilla <junsantilla@live.com>
2023-11-25 21:04:34 +0800

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
-"use client";
-import "./globals.css";
-import { AuthContextProvider } from "../context/AuthContext";
+"use client"
+import "./globals.css"
+import { AuthContextProvider } from "../context/AuthContext"
+import { Analytics } from "@vercel/analytics/react"
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en">
-			<AuthContextProvider>
-				<body>{children}</body>
-			</AuthContextProvider>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const isLocalhost = process.env.NODE_ENV === "development"
+
+    return (
+        <html lang="en">
+            <AuthContextProvider>
+                <body>
+                    {children}
+
+                    {/* Render Analytics only if not in localhost */}
+                    {!isLocalhost && <Analytics />}
+                </body>
+            </AuthContextProvider>
+        </html>
+    )
 }
